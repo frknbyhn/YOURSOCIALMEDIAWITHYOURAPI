d023f8fbd68ad13d01822985d8f0aba5f7763547
Fuko <furkanbeyhan@furkan-macbook-pro.local>
2019-03-01 14:14:06 +0300

    Initial Commit

---
+++ NotificiationList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificiationList
+//
+//  Created by Furkan Beyhan on 1.03.2019.
+//  Copyright © 2019 Furkan Beyhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
